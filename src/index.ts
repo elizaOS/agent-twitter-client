e33e625e896308e79a30951a67761684adeb0c70
Sayo <hi@sayo.wtf>
2025-01-14 00:40:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 export { Scraper } from './scraper';
 
 export * from '@/cli';
-// Not sure if this is needed
-// export * from '@/core';
+export * from '@/core';
 export * from '@/features';
 export * from '@/scraper';
 export * from '@/spaces';
